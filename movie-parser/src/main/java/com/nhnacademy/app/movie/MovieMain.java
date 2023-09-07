083a3463190796f4019c079fd9f0b5550dd18f5b
Jeongyong Lee <hua9378@gmail.com>
2023-09-07 19:44:00 +0900

    chore: 프로젝트 생성

---
+++ movie-parser/src/main/java/com/nhnacademy/app/movie/MovieMain.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.app.movie;
+
+import java.util.List;
+
+public class MovieMain {
+
+    public static void main(String[] args) {
+        MovieParser movieParser = new BasicMovieParser();
+
+        try {
+            List<Movie> movieList = movieParser.parse("movies.csv");
+            for (Movie movie : movieList) {
+                System.out.println(movie);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
