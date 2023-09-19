c7c57754061d5dc1401a784c96fe30d03bfac185
Jeongyong Lee <hua9378@gmail.com>
2023-09-19 13:11:12 +0900

    feat: ServerSocket 연습

---
+++ network-practice/src/main/java/com/nhnacademy/quiz/Quiz08.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.quiz;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Quiz08 {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12345);
+        Socket client = serverSocket.accept();
+
+        System.out.printf("Client [%s]가 연결되었습니다.%n", client.getRemoteSocketAddress());
+    }
+}
---
+++ network-practice/src/main/java/com/nhnacademy/quiz/Quiz09.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.quiz;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Quiz09 {
+
+    public static void main(String[] args) {
+        int port = 1234;
+        if (args.length != 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(port);
+                Socket client = serverSocket.accept();
+                BufferedReader bufferedReader =
+                        new BufferedReader(new InputStreamReader(client.getInputStream()));
+                BufferedWriter bufferedWriter =
+                        new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));) {
+
+            process(bufferedReader, bufferedWriter);
+
+        } catch (IOException ignore) {
+        }
+    }
+
+    private static void process(BufferedReader bufferedReader, BufferedWriter bufferedWriter)
+            throws IOException {
+        bufferedWriter.write(bufferedReader.readLine());
+        bufferedWriter.flush();
+    }
+}
