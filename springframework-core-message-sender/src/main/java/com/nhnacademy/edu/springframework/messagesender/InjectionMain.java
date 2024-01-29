3817ce56abe5d90fe61f89dc7811f6e49b85f6da
Jeongyong Lee <hua9378@gmail.com>
2024-01-29 17:06:21 +0900

    Setter injection

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/InjectionMain.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/InjectionMain.java
@@ -7,13 +7,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class InjectionMain {
     private static final String EMAIL = "aiot0127@nhnacademy.com";
     private static final String PHONE_NUMBER = "010-0000-0000";
+    private static final String BEANS_XML = "beans.xml";
     private static final String MESSAGE_SEND_SERVICE = "messageSendService";
     private static final String MESSAGE = "테스트";
 
     public static void main(String[] args) {
         User user = new User(EMAIL, PHONE_NUMBER);
 
-        try (ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("beans.xml")) {
+        try (ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(BEANS_XML)) {
             classPathXmlApplicationContext.getBean(MESSAGE_SEND_SERVICE, MessageSendService.class)
                     .doSendMessage(user, MESSAGE);
         }
