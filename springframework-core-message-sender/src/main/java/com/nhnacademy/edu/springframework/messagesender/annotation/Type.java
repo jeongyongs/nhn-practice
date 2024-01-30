6db25e810c63a9f6a0223835851065ce43840bde
Jeongyong Lee <hua9378@gmail.com>
2024-01-30 10:54:19 +0900

    Qualifier

---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/annotation/Type.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.edu.springframework.messagesender.annotation;
+
+public enum Type {
+    SMS, EMAIL
+}
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.edu.springframework.messagesender.service;
 
+import com.nhnacademy.edu.springframework.messagesender.annotation.SMS;
 import com.nhnacademy.edu.springframework.messagesender.domain.User;
 import com.nhnacademy.edu.springframework.messagesender.sender.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MessageSendService {
     private MessageSender messageSender;
@@ -10,14 +12,15 @@ public class MessageSendService {
         // default constructor
     }
 
-    public MessageSendService(MessageSender messageSender) {
+    @Autowired
+    public MessageSendService(@SMS MessageSender messageSender) {
         this.messageSender = messageSender;
-        System.out.println("Constructor");
+        System.out.println("MessageSendService constructor has been invoked!");
     }
 
-    public void setSmsMessageSender(MessageSender messageSender) {
+    public void setMessageSender(MessageSender messageSender) {
         this.messageSender = messageSender;
-        System.out.println("Setter");
+        System.out.println("MessageSendService setter has been invoked!");
     }
 
     public void doSendMessage(User user, String message) {
